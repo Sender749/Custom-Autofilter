94d052fbca6bbf03bb94effae074fc115f73bcc6
Sender749 <jotmahal2003@gmail.com>
2026-02-23 22:29:36 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -326,16 +326,6 @@ async def _process_with_lock(bot, filename, caption, media_info, base_name, proc
         )
         movie_doc["files"].append(file_data)
         schedule_update(bot, base_name)
-        
-async def auto_delete_update(bot, message_id):
-    await asyncio.sleep(172800)  # 2 days = 48*60*60 = 172800 seconds
-    try:
-        await bot.delete_messages(
-            chat_id=MOVIE_UPDATE_CHANNEL,
-            message_ids=message_id
-        )
-    except Exception as e:
-        logger.error(f"Auto delete failed: {e}")
 
 async def send_movie_update(bot, base_name):
     max_retries = 3
@@ -383,7 +373,6 @@ async def send_movie_update(bot, base_name):
                 {"_id": base_name},
                 {"$set": {"message_id": msg.id, "is_photo": is_photo}}
             )
-            asyncio.create_task(auto_delete_update(bot, msg.id))
             return msg
         except FloodWait as e:
             wait_time = e.value + 2
