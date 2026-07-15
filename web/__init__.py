a7a8b71952cdaf0bbaefb63d4e01b9f6a8ceaf49
Sender749 <jotmahal2003@gmail.com>
2026-07-15 20:02:16 +0530

    Update __init__.py

--- web/__init__.py
+++ web/__init__.py
@@ -1,7 +1,9 @@
 from aiohttp import web
 from web.stream_routes import routes as stream_routes
 from web.miniapp_routes import routes as miniapp_routes
+from web.netflix_routes import routes as netflix_routes
 
 web_app = web.Application()
 web_app.add_routes(stream_routes)
 web_app.add_routes(miniapp_routes)
+web_app.add_routes(netflix_routes)
